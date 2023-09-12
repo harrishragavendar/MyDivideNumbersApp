a27b5731242f54ece88f5f84fc09cccdd22c8442
user name <you@example.com>
2023-09-13 00:02:33 +0530

    Exception feature added

--- DivideNumbers.java
+++ DivideNumbers.java
@@ -1,13 +1,26 @@
-public class DivideNumbers {
-    public static double divide(int num1, int num2) {
-        double quotient = num1 / num2;
-        return quotient;
+class DivisionByZeroException extends Exception {
+    public DivisionByZeroException() {
+        super("Error: Division by zero is not allowed.");
     }
+}
+
+public class DivideNumbers {
+    public static double divide(int num1, int num2) throws DivisionByZeroException {
+        if (num2 == 0) {
+            throw new DivisionByZeroException();
+        }
+        return (double) num1 / num2;
+    }
+
     public static void main(String[] args) {
         int num1 = 50;
-        int num2 = 5;
-        double quotient = divide(num1, num2);
-        System.out.println("Quotient using method: " + quotient);
-        System.out("iuiu")
+        int num2 = 0; 
+
+        try {
+            double quotient = divide(num1, num2);
+            System.out.println("Quotient: " + quotient);
+        } catch (DivisionByZeroException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
