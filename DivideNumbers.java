78bf04eb6c235dece44cca7e91e033d3c3a7491a
user name <you@example.com>
2023-09-12 23:54:35 +0530

    Method added

--- DivideNumbers.java
+++ DivideNumbers.java
@@ -1,8 +1,12 @@
 public class DivideNumbers {
+    public static double divide(int num1, int num2) {
+        double quotient = num1 / num2;
+        return quotient;
+    }
     public static void main(String[] args) {
         int num1 = 50;
         int num2 = 5;
-        double quotient = num1 / num2;
-        System.out.println("Quotient: " + quotient);
+        double quotient = divide(num1, num2);
+        System.out.println("Quotient using method: " + quotient);
     }
 }
